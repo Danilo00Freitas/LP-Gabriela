588b0b7e00c1a2e1c993af31de56de08896d5a9b
Danilo Freitas <danilosfreitass@gmail.com>
2025-03-16 14:07:46 -0300

    limpeza nos comentários do js

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,10 @@
 window.addEventListener("DOMContentLoaded", (event) => {
-  // Navbar shrink function (Comportamento atual para telas maiores que 992px)
+  // Função para encolher a navbar em telas maiores que 992px
   var navbarShrink = function () {
     const navbarCollapsible = document.body.querySelector("#mainNav");
     if (!navbarCollapsible) {
       return;
     }
-    // Para telas maiores que 992px
     if (window.innerWidth > 991) {
       if (window.scrollY === 0) {
         navbarCollapsible.classList.remove("navbar-shrink");
@@ -15,13 +14,10 @@ window.addEventListener("DOMContentLoaded", (event) => {
     }
   };
 
-  // Shrink a navbar quando a página for rolada (funciona para todas as telas)
+  // Chama a função de encolher a navbar
   navbarShrink();
 
-  // Evento de scroll para o comportamento de navbar para telas maiores que 992px
-  document.addEventListener("scroll", navbarShrink);
-
-  //  Ativar o scrollspy do Bootstrap para navegação
+  // Ativa o scrollspy do Bootstrap
   const mainNav = document.body.querySelector("#mainNav");
   if (mainNav) {
     new bootstrap.ScrollSpy(document.body, {
@@ -30,7 +26,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
     });
   }
 
-  // Collapse a navbar responsiva quando o toggler for visível
+  // Colapsa a navbar responsiva quando o toggler é visível
   const navbarToggler = document.body.querySelector(".navbar-toggler");
   const responsiveNavItems = [].slice.call(
     document.querySelectorAll("#navbarResponsive .nav-link")
@@ -43,43 +39,38 @@ window.addEventListener("DOMContentLoaded", (event) => {
     });
   });
 
-  // Funcionalidade para esconder a navbar ao rolar para baixo em telas menores que 992px
+  // Lógica para esconder ou mostrar a navbar em telas menores que 992px
   var lastScrollTop = 0;
   const navbar = document.getElementById("mainNav");
 
-  // Evento de scroll para esconder ou mostrar a navbar em telas menores que 992px
   window.addEventListener("scroll", function () {
     if (window.innerWidth <= 991) {
-      // Somente em telas menores que 992px
       let currentScroll =
         window.pageYOffset || document.documentElement.scrollTop;
 
-      // Se rolou para baixo, esconder a navbar
       if (currentScroll > lastScrollTop) {
-        navbar.style.display = "none"; // Esconde a navbar quando rolar para baixo
+        navbar.style.display = "none";
       } else {
-        navbar.style.display = "flex"; // Exibe a navbar quando rolar para cima ou estiver no topo
+        navbar.style.display = "flex";
       }
 
-      // Atualiza a última posição do scroll
       lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
     }
   });
 
-  // A lógica para mostrar a navbar no topo (comportamento para telas menores)
+  // Lógica para mostrar a navbar no topo em telas menores que 992px
   const navbarTop = function () {
     if (window.innerWidth <= 991) {
-      // Somente em telas menores que 992px
       const navbar = document.getElementById("mainNav");
       if (window.scrollY === 0) {
-        navbar.style.display = "flex"; // Exibe a navbar quando estiver no topo
+        navbar.style.display = "flex";
       } else {
-        navbar.style.display = "none"; // Esconde a navbar quando rolar para baixo
+        navbar.style.display = "none";
       }
     }
   };
 
-  // Chama a função de navbarTop quando a página for carregada ou rolada
+  // Chama a função quando a página for carregada ou rolada
   navbarTop();
   window.addEventListener("scroll", navbarTop);
 });
